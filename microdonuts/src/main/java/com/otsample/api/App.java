e5f96c3c22149cbcfdb46da7745f89c931854099
Harnit Singh <harnit.singh@signalfx.com>
2019-04-11 19:23:10 -0700

    modified tracer endpoint to send to SFx gateway

--- microdonuts/src/main/java/com/otsample/api/App.java
+++ microdonuts/src/main/java/com/otsample/api/App.java
@@ -11,11 +11,6 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
-import com.lightstep.tracer.jre.JRETracer;
-import com.lightstep.tracer.shared.Options;
-
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.Configuration.ReporterConfiguration;
 import io.jaegertracing.Configuration.SamplerConfiguration;
@@ -23,10 +18,6 @@ import io.jaegertracing.Configuration.SenderConfiguration;
 import io.jaegertracing.samplers.ConstSampler;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
-import zipkin2.Span;
-import zipkin2.reporter.AsyncReporter;
-import zipkin2.reporter.Reporter;
-import zipkin2.reporter.okhttp3.OkHttpSender;
 
 public class App
 {
@@ -81,32 +72,13 @@ public class App
                 .withType(ConstSampler.TYPE)
                 .withParam(1);
             SenderConfiguration senderConfig = new SenderConfiguration()
-                .withAgentHost(config.getProperty("jaeger.reporter_host"))
-                .withAgentPort(Integer.decode(config.getProperty("jaeger.reporter_port")));
+                .withEndpoint(config.getProperty("jaeger.endpoint"));
             ReporterConfiguration reporterConfig = new ReporterConfiguration()
                 .withLogSpans(true)
                 .withFlushInterval(1000)
                 .withMaxQueueSize(10000)
                 .withSender(senderConfig);
             tracer = new Configuration(componentName).withSampler(samplerConfig).withReporter(reporterConfig).getTracer();
-        } else if ("zipkin".equals(tracerName)){
-            OkHttpSender sender = OkHttpSender.create(
-                "http://" +
-                    config.getProperty("zipkin.reporter_host") + ":" +
-                    config.getProperty("zipkin.reporter_port") + "/api/v1/spans");
-            Reporter<Span> reporter = AsyncReporter.builder(sender).build();
-            tracer = BraveTracer.create(Tracing.newBuilder()
-                .localServiceName(componentName)
-                .spanReporter(reporter)
-                .build());
-        } else if ("lightstep".equals(tracerName)) {
-            Options opts = new Options.OptionsBuilder()
-                .withAccessToken(config.getProperty("lightstep.access_token"))
-                .withCollectorHost(config.getProperty("lightstep.collector_host"))
-                .withCollectorPort(Integer.decode(config.getProperty("lightstep.collector_port")))
-                .withComponentName(componentName)
-                .build();
-            tracer = new JRETracer(opts);
         } else {
             return false;
         }
